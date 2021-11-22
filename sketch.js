66b0ba722070c85c755de7d926b3a4c0dbdc47bd
DerrianCodes <94866511+DerrianCodes@users.noreply.github.com>
2021-11-22 14:30:11 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var apple;
+
+
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+apple = createSprite(200,200,10,10)
+
+
+
+}
+
+function draw() {
+  background(220);
+
+
+
+  drawSprites()
+}
